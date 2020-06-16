79627977f1dd8e7fac54ba6d9c92f084edb21989
Florin Gologan <florin.gologan@made.com>
2020-06-16 12:23:29 +0300

    Switch to array/object routes

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import Home from './components/Home';
 import ProductsList from './components/ProductsList';
 
-export default () => {
-  return (
-    <div>
-      <Route exact path="/" component={ Home }/>
-      <Route path="/products" component={ ProductsList }/>
-      <Route path="/test" component={ () => 'Test Route' }/>
-    </div>
-  );
-}
+export default [
+  {
+    path: '/',
+    component: Home,
+    exact: true
+  },
+  {
+    path: '/products',
+    component: ProductsList,
+  },
+  {
+    path: '/test',
+    component: () => 'Test route',
+  }
+];
