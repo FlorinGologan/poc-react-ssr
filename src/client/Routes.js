fcc2b0c2674f764a3f58548d33b68469eec096f5
Florin Gologan <florin.gologan@made.com>
2020-06-17 08:09:21 +0300

    404 Page and context support

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from './App';
 import HomePage from './pages/HomePage';
 import ProductsListPage from './pages/ProductsListPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export default [
   {
@@ -11,14 +12,14 @@ export default [
         ...HomePage,
         path: '/',
         exact: true
-      },
-      {
+      }, {
         ...ProductsListPage,
         path: '/products',
-      },
-      {
+      }, {
         path: '/test',
         component: () => <h1>Test route</h1>,
+      }, {
+        ...NotFoundPage
       }
     ]
   }
